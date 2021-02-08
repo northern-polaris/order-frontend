3436eec069e7d5c18a1b70f3e06fe5561d2045ef
Aurora Hoxha <aurorahoxha23@gmail.com>
2021-02-08 20:56:41 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {AgentModule} from './agent/agent.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AgentModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
