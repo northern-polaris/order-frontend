9a697a55e0a2a8fb9ba90ef9e9387c17ff7de83d
Aurora Hoxha <aurorahoxha23@gmail.com>
2021-02-17 21:36:52 +0100

    Seller Form Component Added

---
+++ src/app/agent/seller-form/seller-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SellerFormComponent } from './seller-form.component';
+
+describe('SellerFormComponent', () => {
+  let component: SellerFormComponent;
+  let fixture: ComponentFixture<SellerFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SellerFormComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SellerFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
